d3ed8e0dfa79b6669b01716f469109cef89c4b23
Obaydz <oubeidallahzmander@gmail.com>
2026-07-28 16:14:26 +0100

    Added Speed Insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import { GallerySection } from '@/components/GallerySection';
 import { AboutSection } from '@/components/AboutSection';
 import { ContactSection } from '@/components/ContactSection';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <Hero />
       <MenuSection />
       <EventBookingSection />
